d563afa8a0ff6327ae78c497e89655c5f6460d0c
bemxio <bemxiov@protonmail.com>
2023-01-15 17:29:27 +0100

    implement a user input for choosing the tracks

--- main.py
+++ main.py
@@ -52,13 +52,21 @@ def pitch_change(segment: AudioSegment, note: int, root_note: int = 72) -> Audio
 song = AudioSegment.from_wav("song.wav")
 baseline = mido.MidiFile("baseline.mid")
 
-#print("Select the MIDI track to be used as a baseline:")
-#
-#for index, track in enumerate(baseline.tracks, start=1):
-#    print(f"#{index}: \"{track.name}\"")
-#
-#choice = int(input("Your answer: ")) - 1
-#track = baseline.tracks[choice]
+print("Select the MIDI tracks to be used as a baseline:")
+
+for index, track in enumerate(baseline.tracks, start=1):
+    print(f"#{index}: \"{track.name}\"")
+
+print("\nType out your choice by seperating the choices with a **comma**,")
+print("ex. `3,2,4,6`\n")
+
+choices = input("Your answer: ")
+choices = [int(index) - 1 for index in choices.split(",")]
+
+if not choices:
+    choices = range(len(baseline.tracks))
+ 
+tracks = [baseline.tracks[index] for index in choices]
 
 ticks_per_beat = baseline.ticks_per_beat
 tempo = get_tempo(baseline)
@@ -70,7 +78,7 @@ samples = [
     (AudioSegment.from_wav(f"samples/sample3.wav"), 81 - 12)
 ]
 
-for track in baseline.tracks:
+for track in tracks:
     longest = get_longest_note(track) / len(samples)
     notes = {}
     time = 0
@@ -108,45 +116,5 @@ for track in baseline.tracks:
 
         print(f"Added note {message.note} at position {position} to the song")
         notes.pop(message.note)
-
-"""
-longest = get_longest_note(track) / len(samples)
-notes = {}
-time = 0
-
-for message in track:
-    time += message.time
-
-    if message.type not in ("note_on", "note_off"):
-        continue
-    
-    if message.type == "note_on" and message.velocity > 0:
-        notes[message.note] = time
-        continue
-
-    position = notes.get(message.note, 0)
-
-    length = time - position
-    seconds = mido.tick2second(length, ticks_per_beat, tempo)
-
-    if seconds <= longest:
-        sample, root_note = samples[0]
-    elif seconds <= longest * 2:
-        sample, root_note = samples[1]
-    else:
-        sample, root_note = samples[2]
-
-    multiplier = len(sample) / (seconds * 1000)
-
-    sample = pitch_change(sample, message.note, root_note=root_note - 12)
-    sample = speed_change(sample, multiplier)
-
-    position = int(mido.tick2second(position, ticks_per_beat, tempo) * 1000)
-
-    song = song.overlay(sample, position=position)
-
-    #print(f"Added note {message.note} at position {position} to the song")
-    notes.pop(message.note)
-"""
 
 song.export("final.wav", format="wav")
